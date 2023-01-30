d3adf26fe658e6ba3904537aab5f4e602923348e
Collins Ogbodo <coogbodo1@sheffield.ac.uk>
2023-01-30 13:43:45 +0000

    Data preprocessing code
    Isolation of the data preprocessing

---
+++ Data-Preprocessing.py
@@ -0,0 +1,77 @@
+
+iters = [1]
+reps = [1]
+test_series = "BR_AR"
+
+def Data_Preprocess(iters, reps, test_series):
+    #Import and process FRF data
+    import os,pickle,zipfile
+    #empty list for individual sensor plot
+    sensor_frf_lists = {}
+    sensor_frf_freq_lists = {}
+    sensor_frf_mean = {}
+    
+    for iter_ in iters:
+        for rep in reps: 
+            zip_file_path = "Test_Data/" + test_series + "/"+ test_series \
+                + "_" + str(iter_)+ '/' + test_series + "_" \
+                    + str(iter_) + "_" + str(rep) + '.zip'
+            #directory for dumping the extraction file
+            ext_dump_dir = 'Extraction_Dump'
+            if not os.path.exists(ext_dump_dir):
+                 os.makedirs(ext_dump_dir)
+            #unzip files 
+            with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+                zip_ref.extractall(ext_dump_dir)
+    
+            #create a list of files
+            list_files = os.listdir(ext_dump_dir)
+            list_files.remove('meta_data.json')
+            
+            #Remove FRC which is an empty list
+            list_files.remove('FRC.pickle')
+            
+            #Data dictionary label
+            data_labels = [label[:-7] for label in list_files]
+            data_labels = [label.replace("-", "_" ) for label in data_labels]
+            
+            #create an empty list for frf and coh and their frequencies for each sensor
+            if not sensor_frf_lists:
+                sensor_frf_lists = {key:[] for key in list_files}
+                sensor_frf_freq_lists = {key:[] for key in list_files}
+    
+            #create combine all repetition for each sensor
+            for sensor in list_files:
+                #open file
+                infile = open(os.path.join(ext_dump_dir,sensor), 'rb')
+                sensor_ = pickle.load(infile)
+                infile.close()
+                
+                all_data = sensor_['data']
+                #extract individual data
+                frf= all_data[-1]
+                
+                #zip all data with corresponding frequencies
+                frf_data, frf_freq = frf['data_y'],frf['data_x']
+                
+                #Append the zipped data in a list for each sensors
+                sensor_frf_lists[sensor].append(frf_data)
+                sensor_frf_freq_lists[sensor].append(frf_freq) 
+         
+        # Empty dic. for all iteration of individual sensor
+        if not sensor_frf_mean:
+            sensor_frf_mean = {key:[] for key in data_labels}
+            sensor_frf_freq_mean = {key:[] for key in data_labels}
+            
+        for sensors in sensor_frf_lists:
+            frf = sensor_frf_lists[sensors]
+            
+            #find average of all iterations
+            frf_average = [sum(frf_list) / len(frf_list) for frf_list in zip(*sensor_frf_lists[sensors])]
+            frf_freq_average = [sum(frf_freq_list) / len(frf_freq_list) for frf_freq_list in zip(*sensor_frf_freq_lists[sensors])]
+            sensor = sensors[:-7]
+            sensor = sensor.replace('-','_')
+            #Store average for this sensor in the empty dictionary
+            sensor_frf_mean[sensor] = frf_average
+            sensor_frf_freq_mean[sensor] = frf_freq_average
+    return sensor_frf_mean, sensor_frf_freq_mean
