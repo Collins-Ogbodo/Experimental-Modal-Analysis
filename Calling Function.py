e549321b96d5286264117631d39f3fae00369544
Collins Ogbodo <coogbodo1@sheffield.ac.uk>
2023-01-30 18:47:16 +0000

    Adding GRFPM in the calling file

--- Calling Function.py
+++ Calling Function.py
@@ -1,5 +1,6 @@
 from DataPreprocessing import DataPrep
 from Rational_Polynomial_Fraction_Method import RFPM
+from Global_Rational_Polynomial_Fraction_Method import GRFPM
 from StabilizationDiagram import StabDia
 
 #Data Preprocessing
@@ -15,12 +16,25 @@ nat_freqs =[]
 damp_ratio =[]
 order = []
 frf_est = []
+nat_freqs_G =[]
+damp_ratio_G =[]
+Order_G = []
+frf_est_G = []
 for i in N:
-    nat_freq_, dp, order_, FRF, Freq, FRF_est_ = RFPM(frf, freq, 30, 50, sensor_name, i)
+    nat_freq_, dp, order_, FRF, Freq, FRF_est_ = RFPM(frf, freq, 47, 55, sensor_name, i)
+    nat_freq_, dp, order_, FRF_G, Freq, FRF_est_ = GRFPM(frf, freq, 47, 55, i)
     nat_freqs.append(nat_freq_)
     order.append(order_)
     damp_ratio.append(dp)
     frf_est.append(FRF_est_)
+    nat_freqs_G.append(nat_freq_)
+    Order_G.append(order_)
+    damp_ratio_G.append(dp)
+    frf_est_G.append(FRF_est_)
+
 
 #Plot the stabilization Diagram    
+plot = StabDia(nat_freqs_G, FRF_G,frf_est_G, Freq, Order_G, 'Global-RFPM')
 plot = StabDia(nat_freqs, FRF,frf_est, Freq, order, sensor_name)
+
+
