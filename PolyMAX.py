f6b6ba51609784e08bd54a661e23ae1dc0325f2f
Collins Ogbodo <coogbodo1@sheffield.ac.uk>
2023-01-31 16:07:08 +0000

    Correcting FRF Matrix

--- PolyMAX.py
+++ PolyMAX.py
@@ -13,12 +13,11 @@ def PolyMAX(FRF, Freq, min_freq, max_freq, N):
     FRF = pd.DataFrame(FRF)
     #convert dataframe to numpy arrary for splitting ease.
     FRF.to_numpy()
+    FRF = np.transpose(FRF)
     #Extract the number of output
     m = FRF.shape[1]
     #Creating the FRF Matrix
     FRF = [list(frfs) for frfs in FRF.itertuples(index=False, name=None)]
-    H = FRF
-    print(FRF)
     #Because the frequency for all sensors is same whe just select the first.
     Freq = Freq['EXH']
     # Find corresponding indices of frequency range
@@ -26,32 +25,39 @@ def PolyMAX(FRF, Freq, min_freq, max_freq, N):
     imax = Freq.index(max_freq) 
     #Frequency and FRF range
     Freq = Freq[imin:imax]
-    FRF = FRF[imin:imax]
     #Length of frequency
     Nf = len(Freq)
     #compute the sample time
     f_0 = min(Freq); f_N = max(Freq)
     dt = 1/(2*(f_N-f_0))
-    X_sensor = []
-    for i in range(Nf):
-        Omega = []
-        for j in range(0,N+1):
-            omega = np.exp(1j * Freq[i] * dt * j)
-            Omega.append(omega)
-        W_k = 1/Freq[i] if Freq[i] != 0.0 else 0.0
-        X_ = np.multiply(W_k,Omega)
-        X_sensor.append(X_)
-    Y_sensor = np.kron(-X_,FRF)
-    R_sensor = np.real(np.dot(np.transpose(np.conjugate(X_sensor)), X_sensor))
-    T_sensor = np.real(np.dot(np.transpose(np.conjugate(Y_sensor)), Y_sensor))
-    S_sensor = np.real(np.dot(np.transpose(np.conjugate(X_sensor)), Y_sensor)) 
-    M_sensor = T_sensor - (np.transpose(np.conjugate(S_sensor)) @ np.linalg.inv(R_sensor) @ S_sensor)
+    #Define initial matrix for M
+    M = np.zeros([N+1,N+1])
+    for frfs in FRF:
+        frfs = frfs[imin:imax]
+        frfs = np.c_[frfs]
+        X_sensor = []
+        for i in range(Nf):
+            Omega = []
+            for j in range(0,N+1):
+                omega = np.exp(1j * Freq[i] * dt * j)
+                Omega.append(omega)
+            W_k = 1/Freq[i] if Freq[i] != 0.0 else 0.0
+            X_ = np.multiply(W_k,Omega)
+            X_sensor.append(X_)
+        #Repeat for number of input and comput the Y columns
+        for 
+        Y_sensor = np.kron(-X_,frfs)
+        R_sensor = np.real(np.dot(np.transpose(np.conjugate(X_sensor)), X_sensor))
+        T_sensor = np.real(np.dot(np.transpose(np.conjugate(Y_sensor)), Y_sensor))
+        S_sensor = np.real(np.dot(np.transpose(np.conjugate(X_sensor)), Y_sensor)) 
+        M_sensor = T_sensor - (np.transpose(np.conjugate(S_sensor)) @ np.linalg.inv(R_sensor) @ S_sensor)
+        M =+ M_sensor
     M_sensor = 2* M_sensor
-    alpha = -1*np.linalg.inv(M[0:N*m,0:N*m]) @ M[0:N*m, N*m:len(M)]
+    alpha = -1*np.linalg.inv(M_sensor[0:N*m,0:N*m]) @ M_sensor[0:N*m, N*m:len(M_sensor)]
     Im = np.identity(m)
     alpha = np.concatenate((alpha, Im), axis = 0)
     #creatin the companion matrix
-    CM = np.eye(len(M),k = 1)
+    CM = np.eye(len(M_sensor),k = 1)
     #remove the last row and join with alpha
     CM = CM[0:-1,:]
     CM = np.concatenate((CM, np.transpose(alpha)), axis = 0)
