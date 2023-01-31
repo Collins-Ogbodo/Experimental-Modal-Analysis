3c35bf82ec15a4afa74d1673f419740e277acf59
CollinsJnr-001 <117471619+CollinsJnr-001@users.noreply.github.com>
2023-01-31 17:29:00 +0000

    Modification of Loops and optimization

--- PolyMAX.py
+++ PolyMAX.py
@@ -9,15 +9,6 @@ def PolyMAX(FRF, Freq, min_freq, max_freq, N):
     Frequency value as list Sensor input must be a the string of 
     the sensor name"""
     #N - degree of freedom
-    #convert dictionary FRF to DataFrame
-    FRF = pd.DataFrame(FRF)
-    #convert dataframe to numpy arrary for splitting ease.
-    FRF.to_numpy()
-    FRF = np.transpose(FRF)
-    #Extract the number of output
-    m = FRF.shape[1]
-    #Creating the FRF Matrix
-    FRF = [list(frfs) for frfs in FRF.itertuples(index=False, name=None)]
     #Because the frequency for all sensors is same whe just select the first.
     Freq = Freq['EXH']
     # Find corresponding indices of frequency range
@@ -29,46 +20,72 @@ def PolyMAX(FRF, Freq, min_freq, max_freq, N):
     Nf = len(Freq)
     #compute the sample time
     f_0 = min(Freq); f_N = max(Freq)
+    #Sampling Time
     dt = 1/(2*(f_N-f_0))
     #Define initial matrix for M
     M = np.zeros([N+1,N+1])
-    for frfs in FRF:
-        frfs = frfs[imin:imax]
-        frfs = np.c_[frfs]
-        X_sensor = []
-        for i in range(Nf):
-            Omega = []
-            for j in range(0,N+1):
-                omega = np.exp(1j * Freq[i] * dt * j)
-                Omega.append(omega)
-            W_k = 1/Freq[i] if Freq[i] != 0.0 else 0.0
-            X_ = np.multiply(W_k,Omega)
-            X_sensor.append(X_)
-        #Repeat for number of input and comput the Y columns
-        for 
-        Y_sensor = np.kron(-X_,frfs)
-        R_sensor = np.real(np.dot(np.transpose(np.conjugate(X_sensor)), X_sensor))
+    #Number ouput
+    l = len(FRF)
+    #number input
+    m = len(FRF[0])
+    X = []
+    for i in range(Nf):
+        Omega = []
+        for j in range(0,N+1):
+            omega = np.exp(1j * Freq[i] * dt * j)
+            Omega.append(omega)
+        #print(Freq[i])
+        W_k = 1/Freq[i] if Freq[i] != 0.0 else 0.0
+        X_sensor = np.multiply(W_k,Omega)
+        X.append(X_sensor)
+    #compute M for all output
+    for ls in range(l):
+        #Repeat for number of input and compute the Y columns
+        H = np.zeros([Nf,m])
+        for ms in m:
+            H_sensor = FRF[ls][ms]
+            H_sensor = H_sensor[imin:imax]
+            H[:,ms] =+ H_sensor 
+        Y_sensor = np.kron(-X,H)
+        R_sensor = np.real(np.dot(np.transpose(np.conjugate(X)), X))
         T_sensor = np.real(np.dot(np.transpose(np.conjugate(Y_sensor)), Y_sensor))
-        S_sensor = np.real(np.dot(np.transpose(np.conjugate(X_sensor)), Y_sensor)) 
+        S_sensor = np.real(np.dot(np.transpose(np.conjugate(X)), Y_sensor)) 
         M_sensor = T_sensor - (np.transpose(np.conjugate(S_sensor)) @ np.linalg.inv(R_sensor) @ S_sensor)
         M =+ M_sensor
-    M_sensor = 2* M_sensor
-    alpha = -1*np.linalg.inv(M_sensor[0:N*m,0:N*m]) @ M_sensor[0:N*m, N*m:len(M_sensor)]
-    Im = np.identity(m)
-    alpha = np.concatenate((alpha, Im), axis = 0)
+    M = 2* M
+    #alpha = -1*np.linalg.inv(M[0:N*m,0:N*m]) @ M[0:N*m, N*m:len(M)]
+    #Im = np.identity(m)
+    #alpha = np.concatenate((alpha, Im), axis = 0)
     #creatin the companion matrix
-    CM = np.eye(len(M_sensor),k = 1)
+    #CM = np.eye(len(M),k = 1)
     #remove the last row and join with alpha
-    CM = CM[0:-1,:]
-    CM = np.concatenate((CM, np.transpose(alpha)), axis = 0)
+    #CM = CM[0:-1,:]
+    #CM = np.concatenate((CM, np.transpose(alpha)), axis = 0)
     #poles, PF = np.linalg.eig(CM)
     #poles = np.log(poles)/dt
     #stable poles
     #poles = [pole for pole in poles if np.real(pole) < 0.0 and np.imag(pole) > 0.0]
     #Compuring Natural Frequency
     #w_n = [abs(pole) for pole in poles]
-    return M_sensor, alpha, CM,H
-    
+    return M
+  
+#%%
+
+
+def PolyMaxDataPrep(frf):
+    """This function is pecific to the data sent available, it coverts the two dimensional 
+    space data (SIMO) which is a dictionary with keys equal to the sensor (output)
+    and vales equal to a list of each input across all frequencies to a three dimensional
+    matix space
+    e.g [[[FRF(w) for input 1 for ouput 1 ], [FRF(w) for input 2 for ouput 1 ], ...],
+         [[FRF(w) for input 1 for ouput 2 ], [FRF(w) for input 2 for ouput 2 ], ...]], ...]"""
+    FRF = []
+    for i in frf.values():
+        dump_dim = []
+        dump_dim.append(i)
+        FRF.append(dump_dim)
+    return FRF
+          
 #%%
 from DataPreprocessing import DataPrep
 #Data Preprocessing
@@ -77,7 +94,12 @@ reps = [1]
 test_series = "BR_AR"
 frf, freq = DataPrep(iters, reps, test_series)
 
-M, alpha, cm, H = PolyMAX(frf, freq, 47, 55, 20)
+fRF = PolyMaxDataPrep(frf)
+
+
+#%%
+
+M = PolyMAX(fRF, freq, 47, 55, 20)
 
 
 
