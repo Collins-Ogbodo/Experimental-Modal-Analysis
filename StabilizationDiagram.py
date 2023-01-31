8ed2c086e75128cd5513187357de8763c3bb8cd2
Collins Ogbodo <coogbodo1@sheffield.ac.uk>
2023-01-31 23:55:30 +0000

    adding the curve reconstruction option

--- StabilizationDiagram.py
+++ StabilizationDiagram.py
@@ -1,4 +1,4 @@
-def StabDia(NatFreq, FRF, FRF_est, Freq, Order, sensor):
+def StabDia(NatFreq, FRF, FRF_est, Freq, Order, sensor, recon = 'yes'):
     import matplotlib.pyplot as plt 
     # Create figure and subplot
     fig, host = plt.subplots(figsize=(15,10))   
@@ -11,10 +11,15 @@ def StabDia(NatFreq, FRF, FRF_est, Freq, Order, sensor):
     color1 = plt.cm.viridis(0)
     FRF = [abs(frf) for frf in FRF]
     host.semilogy(Freq, FRF, color=color1, label="FRF")
-    for i, j in zip(FRF_est, Order):
-        k = [abs(frf) for frf in i]
-        host.semilogy(Freq, k)
-    host.legend(Order, loc="upper left")
+    if recon == 'yes':
+        for i, j in zip(FRF_est, Order):
+            k = [abs(frf) for frf in i]
+            host.semilogy(Freq, k)
+        host.legend(Order, loc="upper left")
+    elif recon == 'no':
+        pass
+    else:
+        print('recon = yes/no')
     for w_n, N in zip(NatFreq,Order):
         N= [N for i in range(len(w_n))]
         par1.plot(w_n, N,"+", markersize=5, label='gaga')
