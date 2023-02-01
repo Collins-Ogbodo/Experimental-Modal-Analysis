68b709cd3022669d4b35930c51f1a470a57f031a
Collins Ogbodo <coogbodo1@sheffield.ac.uk>
2023-02-01 10:16:29 +0000

    Splitting the methods

--- Calling Function.py
+++ Calling Function.py
@@ -15,20 +15,46 @@ frf, freq = DataPrep(iters, reps, test_series)
 N = [i for i in range(2,30)]
 min_freq = 20
 max_freq = 60
-
+#%%
 #RFPM parameters
 sensor_name = 'EXH'
 nat_freqs =[]
 damp_ratio =[]
 order = []
 frf_est = []
-
+#OMA for multiple order  nat_freq, dam_ratio, N, FRF, Freq
+for i in N:
+    wn, dp, Order, FRF, Freq, FRF_est = RFPM(frf, freq, min_freq, max_freq, sensor_name, i)
+    #Natural frequency
+    nat_freqs.append(wn)
+    #Damping Ratio
+    damp_ratio.append(dp)
+    #order from method
+    order.append(order)
+    #Estimated FRF
+    frf_est.append(FRF_est)
+#Plot the stabilization Diagram    
+plot = StabDia(nat_freqs, FRF,frf_est, Freq, order, sensor_name, 'yes')
+#%%
 #GRFPM parameters
 nat_freqs_G =[]
 damp_ratio_G =[]
 order_G = []
 frf_est_G = []
-
+#OMA for multiple order  nat_freq, dam_ratio, N, FRF, Freq
+for i in N:
+    wn_G, dp_G, Order_G, FRF_G, _, FRF_est_G = GRFPM(frf, freq, min_freq, max_freq, i)
+    #Natural frequency
+    nat_freqs_G.append(wn_G)   
+    #Damping Ratio
+    damp_ratio_G.append(dp_G)
+    #order from method
+    order_G.append(order_G)
+    #Estimated FRF
+    frf_est_G.append(FRF_est_G)
+#Plot the stabilization Diagram    
+plot = StabDia(nat_freqs_G, FRF_G,frf_est_G, Freq, Order_G, 'Global-RFPM', 'yes')
+#%%
 #Polymax
 fRF = PolyMaxDataPrep(frf)
 nat_freqs_P =[]
@@ -37,28 +63,11 @@ order_P = []
 
 #OMA for multiple order  nat_freq, dam_ratio, N, FRF, Freq
 for i in N:
-    wn, dp, Order, FRF, Freq, FRF_est = RFPM(frf, freq, min_freq, max_freq, sensor_name, i)
-    wn_G, dp_G, Order_G, FRF_G, _, FRF_est_G = GRFPM(frf, freq, min_freq, max_freq, i)
     wn_P, dp_P, Order_P, _, _ = PolyMAX(fRF, freq, min_freq, max_freq, i)
     #Natural frequency
-    nat_freqs.append(wn)
-    nat_freqs_G.append(wn_G)  
     nat_freqs_P.append(wn_P)  
     #Damping Ratio
-    damp_ratio.append(dp)
-    damp_ratio_G.append(dp_G)
-    damp_ratio_P.append(dp_P)
-    #order from method
-    order.append(order)
-    order_G.append(order_G)
-    order_P.append(order_P)
-    #Estimated FRF
-    frf_est.append(FRF_est)
-    frf_est_G.append(FRF_est_G)
-
 #Plot the stabilization Diagram    
-plot = StabDia(nat_freqs, FRF,frf_est, Freq, order, sensor_name, 'yes')
-plot = StabDia(nat_freqs_G, FRF_G,frf_est_G, Freq, Order_G, 'Global-RFPM', 'yes')
 plot = StabDia(nat_freqs_P, FRF_G, _, Freq, order, 'PolyMAX', 'no')
 
 
