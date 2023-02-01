f53a75e9c11b4e66e4f31ebc116a36a80fccd1ee
Collins Ogbodo <coogbodo1@sheffield.ac.uk>
2023-02-01 16:45:00 +0000

    Generalization
    Making the code general for any type of data

--- PolyMaxData.py
+++ PolyMaxData.py
@@ -1,13 +1,13 @@
-def PolyMaxDataPrep(frf):
+def PolyMaxDataPrep(data):
     """This function is pecific to the data sent available, it coverts the two dimensional 
     space data (SIMO) which is a dictionary with keys equal to the sensor (output)
     and vales equal to a list of each input across all frequencies to a three dimensional
     matix space
     e.g [[[FRF(w) for input 1 for ouput 1 ], [FRF(w) for input 2 for ouput 1 ], ...],
          [[FRF(w) for input 1 for ouput 2 ], [FRF(w) for input 2 for ouput 2 ], ...]], ...]"""
-    FRF = []
-    for i in frf.values():
+    DATA = []
+    for i in data.values():
         dump_dim = []
         dump_dim.append(i)
-        FRF.append(dump_dim)
-    return FRF
+        DATA.append(dump_dim)
+    return DATA
