fbc230f0a18761a1c0ea361ada1b36155cd57237
CollinsJnr-001 <117471619+CollinsJnr-001@users.noreply.github.com>
2023-02-22 15:00:25 +0000

    Output dimension
    changed the output dimension to a numpy array of 3D

--- PolyMaxData.py
+++ PolyMaxData.py
@@ -5,9 +5,19 @@ def PolyMaxDataPrep(data):
     matix space
     e.g [[[FRF(w) for input 1 for ouput 1 ], [FRF(w) for input 2 for ouput 1 ], ...],
          [[FRF(w) for input 1 for ouput 2 ], [FRF(w) for input 2 for ouput 2 ], ...]], ...]"""
-    DATA = []
-    for i in data.values():
-        dump_dim = []
-        dump_dim.append(i)
-        DATA.append(dump_dim)
-    return DATA
+    import pandas as pd
+    import numpy as np
+    #Converting file to data frame and array
+    data = pd.DataFrame(data)
+    data = data.to_numpy()
+    #Converting data from 2D [Nf*l] to 3D [m*NF*l]
+    data = np.expand_dims(data, axis= 0)
+    return data
+# =============================================================================
+#     DATA = []
+#     for i in data.values():
+#         dump_dim = []
+#         dump_dim.append(i)
+#         DATA.append(dump_dim)
+#     return DATA
+# =============================================================================
