62ee61e2d8b2792fc535c7aa63ca66baa48dfd70
CollinsJnr-001 <117471619+CollinsJnr-001@users.noreply.github.com>
2023-02-23 20:29:15 +0000

    Coherence_Weight

--- PolyMAXFFT.py
+++ PolyMAXFFT.py
@@ -16,7 +16,7 @@ def PolyMAXFFT(FRF, Freq, Coh, min_freq, max_freq, Nmin, Nmax):
     #Frequency, Coh and FRF range
     Freq = Freq[imin:imax]
     FRF = FRF[:, imin:imax, :]
-    #Coh = Coh[:, imin:imax, :]
+    Coh = Coh[:, imin:imax, :]
     #Length of frequency-Nf, number of input-m, number of output-l
     m, Nf, l = np.shape(FRF)
     #compute the sample time
@@ -25,24 +25,18 @@ def PolyMAXFFT(FRF, Freq, Coh, min_freq, max_freq, Nmin, Nmax):
     dt = 1/(2*(f_N-f_0))
     #Define initial matrix for M, X, Y, for maximum model order 0 to N
     M = np.zeros([Nmax+1,Nmax+1], dtype=complex)
-    X = np.zeros([Nf, Nmax+1], dtype=complex)
-    Y = np.zeros([Nf, (Nmax+1)*m], dtype=complex)
-    #Scalar weighting function
-    #W_k = 1#np.array([1/freq_val if freq_val != 0.0 else 0.0 for freq_val in Freq])
-    #W_k = 100 #Coh[ms][i][ls]
-    #computing X
-    X = np.fft.fftn(np.ones((Nf,1)), s =(2*Nf,), axes=(0,))
-    R_sensor = toeplitz(np.real(X))
     #Computation of the FFT of R=[Y'*Y]
-    T = np.fft.fftn(np.abs(FRF)**2, s =(2*Nf,2*Nf), axes=(1, 2))
+    T = np.fft.fftn(np.abs(FRF*Coh)**2, s =(2*Nf,2*Nf), axes=(1, 2))
     #Computing Y
-    Y = np.fft.fftn(FRF, s =(2*Nf,2*Nf), axes=(1, 2))
+    #Y = np.fft.fftn(FRF, s =(2*Nf,2*Nf), axes=(1, 2))
     #compute M for all output
     for ls in range(l):
+        X = np.fft.fftn(abs(Coh[0,:,ls]), s =(2*Nf,), axes=(0,))
         T_sensor = toeplitz(np.real(T[:,0:Nmax+1,ls]))
-        S_sensor = toeplitz(-np.real(np.conjugate(X[0:Nmax+1])), -np.real(Y[:,0:Nmax+1,ls])) 
+        S = np.fft.fftn((abs(Coh)**2)*FRF,s =(2*Nf,2*Nf), axes=(1, 2))
+        S_sensor = toeplitz(np.real(S[:,0:Nmax+1,ls]))
+        #S_sensor = toeplitz(-np.real(np.conjugate(X[0:Nmax+1])), -np.real(Y[:,0:Nmax+1,ls])) 
         R_sensor = toeplitz(np.real(X[0:Nmax+1]))
-        print(S_sensor)
         M_sensor = T_sensor - (np.transpose(S_sensor) @ np.linalg.inv(R_sensor) @ S_sensor)
         M = M + M_sensor 
     M = 2*M
@@ -77,8 +71,7 @@ def PolyMAXFFT(FRF, Freq, Coh, min_freq, max_freq, Nmin, Nmax):
         #order from method
         order_P.append(n)  
         M = M[:-1,:-1]
-    return nat_freqs_P, damp_ratio_P,order_P, FRF, Freq
-
+    return nat_freqs_P, damp_ratio_P,order_P, FRF, Freq, imax, imin
 
 
 
