fdee43dc34e1c16a97d3dad4cbca1cb8a28be5c2
CollinsJnr-001 <117471619+CollinsJnr-001@users.noreply.github.com>
2023-04-20 20:13:16 +0100

    AutoEMA

--- Auto-EMA-Savitzky-Bolay.py
+++ Auto-EMA-Savitzky-Bolay.py
@@ -6,11 +6,10 @@ import seaborn as sns
 import numpy as np
 from scipy.signal import savgol_filter
 #Data Preprocessing
-iters = [1]
-reps = [1]
-test_series = "BR_AR"
+iters = [27]
+reps = [1,2,3,4,5,6,7,8,9,10]
+test_series = "DS_CTE"
 frf, freq, coh = DataPrep(iters, reps, test_series)
-#%%
 # Generate noisy data
 min_freq, max_freq = 5, 60
 FRF_df = pd.DataFrame(frf)
@@ -39,6 +38,7 @@ peaks, _ = find_peaks(y_smooth, prominence=(1e-04))
 prominences = peak_prominences(y_smooth, peaks)[0]
 contour_heights = y_smooth[peaks] - prominences
 plt.figure(figsize=(17, 8))
+plt.grid() 
 plt.plot( y_smooth)
 plt.plot(peaks,  y_smooth[peaks], "x")
 plt.vlines( x=peaks, ymin=contour_heights, ymax= y_smooth[peaks])
@@ -47,7 +47,7 @@ Id_peaks = [x[i] for i in peaks]
 output = {}
 incr =freq[1]
 WNs = []
-#%%
+
 for i in Id_peaks:
     label = 'Mode'+str(Id_peaks.index(i)+1)
     label1 = 'STD-Mode'+str(Id_peaks.index(i)+1)
@@ -60,7 +60,7 @@ for i in Id_peaks:
         mmax_freq = i + incr
         errors = {}
         
-        for j in range(1,100):
+        for j in range(1,150):
             wn,_,_,_,_,_,error  = RFPM(frf, freq, mmin_freq, mmax_freq, sensor_name, n_modes=6)
             wn = np.mean(wn)
             if np.isnan(wn):
@@ -73,12 +73,12 @@ for i in Id_peaks:
     output[label].append(np.mean(Wn))
     WNs.append(np.mean(Wn))
     output[label1].append(np.std(Wn))
-#%%   
+
 def FreqSeg(FRF, Freq, seg, test_series):
     import matplotlib.pyplot as plt 
     import numpy as np
     import pandas as pd
-    FRF = np.mean(np.array(list(FRF.values())))
+    FRF = np.mean(np.array(list(FRF.values())), axis=(0))
     # Create figure and subplot
     imin = Freq.index(0.0)
     imax = Freq.index(60)
@@ -101,9 +101,8 @@ def FreqSeg(FRF, Freq, seg, test_series):
        # plt.text(seg[i],seg[i+1], 'Segment'+str(i),  ha='center', va='bottom' )
     #plt.title('Modal Analysis RF-'+ test_series)
     plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1), ncol=10)
-    plt.savefig('Results\\RFPM\\Out-of-band Estimate\\'+test_series+".png")
+    #plt.savefig('Results\\RFPM\\Out-of-band Estimate\\'+test_series+".png")
     plt.show()
     return
-#%%
-
 plot_est = FreqSeg(frf, freq,WNs,test_series)
+
